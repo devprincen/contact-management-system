f400704a1f8f413a6dade071efd4f484e5e78772
Prince Goswami <princenath0757@gmail.com>
2025-01-12 11:20:14 +0530

    some line added in file and modified

--- Contact sys.c
+++ Contact sys.c
@@ -2,67 +2,86 @@
 #include <conio.h>
 #include <stdlib.h>
 
-
-void wellcomePage();
-void menuPage();
-
-struct Person {
+struct User {
+	int id;
 	char name[50];
-	int choice;
+	char email[50];
+	char phone[10];
 };
 
-struct Detail {
-	char name[50];
-	int phone;
-	char email;
-	int choice;
-	char address;
-};
+struct Address{
+	int id;
+	int userId;
+	char title[30];
+	char address[100];
+}
+
+struct User users[];
+int i, size = 0;
+
+void wellcome();
+void home();
 
 int main() {
-
-    wellcomePage();
-    menuPage();
-
-    return 0;
+	wellcome();
 }
 
-void wellcomePage() {
+void wellcome() {
 	clrscr();
-    printf("\n\n\n\n\n\n\n\n\n\n\t\t\t  ********  Wellcome ******* \n");
-    printf("\t\t\t------------------------------\n\n");
-    printf("\n\t\t   Press Enter to continue to the MENU page...\n");
-    getch();
-}
-
-void menuPage(struct Person. person) {
-    clrscr();
-
-    printf("\n\n\t\t\t\t----- MENU ----- \n");
-    printf("\t-----------------------------------------------------------------\n\n");
-    printf("1. Prince Goswami\n");
-	printf("-----------------------------------------------------------------\n\n");
-    printf("2. Mohit Goswami\n");
-	printf("-----------------------------------------------------------------\n\n");
-    printf("3. Anop Goswami\n");
-	printf("-----------------------------------------------------------------\n\n");
-    printf("4. Uttam Goswami\n");
-	printf("-----------------------------------------------------------------\n\n");
-    printf("5. Puran Goswami\n");
-	printf("-----------------------------------------------------------------\n\n");
-    scanf("%d", &choice);
-
-    switch (choice) {
-       case 1:
-	   printf("");
-
-
-
-    }
-    getch();
-
-
-
+	printf("\n\n\n\n\n\n\n\n\n\n\t\t\t  ********  WELLCOME ******* \n");
+	printf("\t\t\t------------------------------\n\n");
+	printf("\n\t\t   Press Enter to continue to the HOME page...\n");
+	getch();
+	clrscr();
 }
 
 
+void home(){
+	struct User user;
+	char choice[4];
+	int try;
+
+	clrscr();
+	printf("\n\n\t\t\t  ********  HOME ******* \n");
+
+	printf("\nId\tName\tEmail\tPhone No.\n";)
+	for(i=0; i<size; i++){
+		user = users[i];
+		printf("%d\t%s\t%s\t%s\n",user.id, user.name, user.email, user.phone);
+	}
+	
+	printf("\n---------------------------------------\n");
+	printf("Enter userId to view and use 'ADD' to add new user: ");
+	scanf("%s", &choice);
+	
+	if(choice == "ADD"){
+		addUser();
+		return;
+	}
+	
+	user = users[((int) choice) - 1]
+	if(user == null){
+		printf("Invalid UserId!! press 1 to try again: ");
+		scanf("%d", &try);
+		
+		if(try != 1){
+			exit(0);
+		}
+		home();
+	}
+	viewUser(user);
+}
+
+void viewUser(struct User user){
+	Id: %d..
+	Name: %s ...
+	
+	...
+	
+	\n\n
+	1. edit 2. goto address 3. delete 4. goto home
+	switch()
+	{
+	...
+	}
+}
