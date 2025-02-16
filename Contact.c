f39efe6297b9fdfd65230040d765940774b5aeed
Prince Goswami <princenath0757@gmail.com>
2025-02-16 19:13:13 +0530

    modified contact-management-system file

--- Contact.c
+++ Contact.c
@@ -3,24 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct User {
-    char name[50];
+struct Address {
     int id;
+    char name[50];
     char email[50];
     char address[100];
     char phone[10];
 };
 
-struct User users[100];
+struct Address addre[100];
 int size = 0;
 
 void wellcome();
 void home();
 void addContact();
 void editContact();
-void viewContact(int userId);
-void findContact(int userId);
-void deleteContact(int userId);
+void viewContact(int addreId);
+void findContact(int addreId);
+void deleteContact(int addreId);
 void loadContacts();
 
 int main() {
@@ -35,7 +35,8 @@ void wellcome() {
     system("cls");
     printf("\n\n\n\n\n\n\n\n\n\n\t\t\t  ********  WELCOME ******* \n");
     printf("\t\t\t------------------------------\n\n");
-    printf("Press 1 to enter HOME page\t\t\t\tPress 2 to EXIT the page\n");
+    printf("     1  HOME page\n\n");
+    printf("     2  EXIT page\n");
     scanf("%d", &choice);
     
     switch (choice) {
@@ -52,7 +53,7 @@ void wellcome() {
 }
 
 void home() {
-    int choice, userId;
+    int choice, addreId;
     
     system("cls");
     while (1) {
@@ -61,7 +62,7 @@ void home() {
         printf("1. Add New Contact: \n\n");
         printf("2. Edit Contact: \n\n");
         printf("3. View Contact: \n\n");
-        printf("4. Find Contact: \n\n");
+        printf("4. List Contacts: \n\n");
         printf("5. Delete Contact: \n\n");
         printf("6. Exit\n");
         printf("========================\n");
@@ -78,22 +79,22 @@ void home() {
                 editContact();
                 break;
             case 3:
-                printf("\n\t\t\t\t========= View Contact =========\n\n");
-                printf("Enter User ID to View Address: ");
-                scanf("%d", &userId);
-                viewContact(userId);
+                printf("\n\t\t\t========= View Contact =========\n\n");
+                printf("Enter Address ID to View Address: ");
+                scanf("%d", &addreId);
+                viewContact(addreId);
                 break;
             case 4:
-                printf("\n\t\t\t\t========= Find Contact =========\n\n");
-                printf("Enter User ID to Find Address: ");
-                scanf("%d", &userId);
-                findContact(userId);
+                printf("\n\t\t\t========= Find Contacts =========\n\n");
+                printf("Enter Address ID to View Address: ");
+                scanf("%d", &addreId);
+                findContact(addreId);
                 break;
             case 5:
-                printf("\n\t\t\t\t========= Delete Contact =========\n\n");
-                printf("Enter User ID to Delete Contact: ");
-                scanf("%d", &userId);
-                deleteContact(userId);
+                printf("\n\t\t\t========= Delete Contact =========\n\n");
+                printf("Enter Address ID to Delete Contact: ");
+                scanf("%d", &addreId);
+                deleteContact(addreId);
                 break;
             case 6:
                 exit(0);
@@ -106,38 +107,40 @@ void home() {
 
 int isValidEmail(char email[]) {
 	return (strchr(email, '@') != NULL && strchr(email, '.') != NULL);
-}
+	}
 
 void addContact() {
     FILE *file;
+    char digit;
     if (size >= 100) {
-        printf("User list is full!\n");
+        printf("Address list is full!\n");
         return;
     }
     
-    printf("\n\t\t\t\t========= Add Contact =========\n\n");
+    printf("\n\t\t\t   ========= Add Contact =========\n\n");
     
     printf("Enter Name:\n");
-    scanf(" %[^\n]", users[size].name);
+    scanf(" %[^\n]", addre[size].name);
     
     printf("Enter your ID: \n");
-    scanf("%d", &users[size].id);
+    scanf("%d", &addre[size].id);
     do {
 	    
     printf("Enter Email: \n");
-    scanf(" %[^\n]", users[size].email);
+    scanf(" %[^\n]", addre[size].email);
     
-    if(!isValidEmail(users[size].email)) {
+    if(!isValidEmail(addre[size].email)) {
 	printf("invalid");    
        }
-      } while(!isValidEmail(users[size].email));
+      } while(!isValidEmail(addre[size].email));
       
     printf("Enter Address: \n");
-    scanf(" %[^\n]", users[size].address);
-    
+    scanf(" %[^\n]", addre[size].address);
+
+
     printf("Enter Phone: \n");
-    scanf(" %[^\n]", users[size].phone);
-    
+    scanf(" %[^\n]", addre[size].phone);	
+ 
     file = fopen("contacts.txt", "a");
     
     if (file == NULL) {
@@ -145,98 +148,79 @@ void addContact() {
         return;
     }
  
-    fprintf(file, "%d,%s,%s,%s\n", users[size].id, users[size].name, users[size].email, users[size].address);
+    fprintf(file, "%d,%s,%s,%s\n", addre[size].id, addre[size].name, addre[size].email, addre[size].address, addre[size].phone);
     
     fclose(file);
     
     size++;
   
-    printf("\nUser Added successfully\n");
+    printf("\nAddress Added successfully\n");
     getch();
-}
-	
-
-void deleteContact(int userId) { 
-    int i, j, found = 0;
-    
-    for (i = 0; i < size; i++) {
- 	       if (users[i].id == userId) {
-            found = 1;
-            for (j = i; j < size - 1; j++) {
-                users[j] = users[j + 1];
-            }
-            size--;
-            printf("User deleted successfully!\n");
-            return;
-        }
-    }
-
-    if (!found) {
-        printf("User with ID %d not found.\n", userId);
-    }
-}
-
-void viewContact(int userId) {
-    int i, found = 0;
-        
-    for (i = 0; i < size; i++) {
-        if (users[i].id == userId) {
-            found = 1;
-            printf("----------------------------------------\n");
-printf("|             View Address             |\n");
-printf("----------------------------------------\n");
-printf("| %-15s: %-19s\n", "Name", users[i].name);
-printf("| %-15s: %-19d\n", "User ID", users[i].id);
-printf("| %-15s: %-19s\n", "Email", users[i].email);
-printf("| %-15s: %-19s\n", "Address", users[i].address);
-printf("| %-15s: %-19s\n", "Phone", users[i].phone);
-printf("----------------------------------------\n");
-printf("\n");
-break;
-        }
-    }
-    if (!found) {
-        printf("User with ID %d not found.\n", userId);
-    }
-}
-
-void findContact(int userId) {
-    viewContact(userId);
 }
 
 void editContact() {
-    int userId, choice, i, found = 0;
+    int addreId, choice, i, found = 0;
     printf("\n\t\t\t\t========= Edit Contact =========\n\n");
 
-    printf("Enter User ID to Edit: ");
-    scanf("\n%d", &userId);
+    printf("Enter Address ID to Edit: ");
+    scanf("\n%d", &addreId);
 
     for (i = 0; i < size; i++) {
-        if (users[i].id == userId) {
+        if (addre[i].id == addreId) {
             found = 1;
     printf("\nSelect what information you want to change: \n\n");
     printf("Press 1 to Edit Name\n");
     printf("2 to Edit Email\n");
     printf("3 to Edit Address\n");
     printf("4 to Edit Phone\n");
+    printf("5 to Change all information\n");
     scanf("%d", &choice);
     switch(choice) {
     	case 1:         
     	    printf("Enter New Name: ");
-            scanf(" %[^\n]", users[i].name);   
+            scanf(" %[^\n]", addre[i].name);   
     	break;
     	case 2:
-            printf("Enter New Email: ");
-            scanf(" %[^\n]", users[i].email);
+        do {
+	    
+            printf("Enter Email: \n");
+            scanf(" %[^\n]", addre[size].email);
+    
+       if(!isValidEmail(addre[size].email)) {
+	    printf("invalid");    
+       }
+       } while(!isValidEmail(addre[size].email));
+       
     	break;
     	case 3:
     	    printf("Enter New Address: ");
-    	    scanf(" %[^\n]", users[i].address);
+    	    scanf(" %[^\n]", addre[i].address);
     	break;
     	case 4:
     	    printf("Enter New Phone: ");
-    	    scanf(" %[^\n]", users[i].phone);
+    	    scanf(" %[^\n]", addre[i].phone);
+
     	break;
+    	case 5:
+ 	    printf("Enter New Name: ");
+            scanf(" %[^\n]", addre[i].name); 
+        do {
+	    
+            printf("Enter Email: \n");
+            scanf(" %[^\n]", addre[size].email);
+    
+        if(!isValidEmail(addre[size].email)) {
+	    printf("invalid");    
+          }
+        } while(!isValidEmail(addre[size].email));
+        
+            printf("Enter New Address: ");
+    	    scanf(" %[^\n]", addre[i].address);
+    	    printf("Enter New Phone: ");
+    	    scanf(" %[^\n]", addre[i].phone);
+    	break;
+    	
+    	     
     	default:
     	  printf("this is not  option, please enter right option!\n");
       }
@@ -244,9 +228,60 @@ void editContact() {
     }
 
     if (!found) {
-        printf("User with ID %d not found.\n", userId);
+        printf("Address with ID %d not found.\n", addreId);
     }
 }
+
+	
+
+void deleteContact(int addreId) { 
+    int i, j, found = 0;
+    
+    for (i = 0; i < size; i++) {
+ 	       if (addre[i].id == addreId) {
+            found = 1;
+            for (j = i; j < size - 1; j++) {
+                addre[j] = addre[j + 1];
+            }
+            size--;
+            printf("Address deleted successfully!\n");
+            return;
+        }
+    }
+
+    if (!found) {
+        printf("Address with ID %d not found.\n", addreId);
+    }
+}
+
+void viewContact(int addreId) {
+    int i, found = 0;
+        
+    for (i = 0; i < size; i++) {
+        if (addre[i].id == addreId) {
+            found = 1;
+            printf("----------------------------------------\n");
+printf("|             View Address             |\n");
+printf("----------------------------------------\n");
+printf("| %-15s: %-19s\n", "Name", addre[i].name);
+printf("| %-15s: %-19d\n", "Address ID", addre[i].id);
+printf("| %-15s: %-19s\n", "Email", addre[i].email);
+printf("| %-15s: %-19s\n", "Address", addre[i].address);
+printf("| %-15s: %-19s\n", "Phone", addre[i].phone);
+printf("----------------------------------------\n");
+printf("\n");
+break;
+        }
+    }
+    if (!found) {
+        printf("Address with ID %d not found.\n", addreId);
+    }
+}
+
+void findContact(int addreId) {
+    viewContact(addreId);
+}
+
 
 void loadContacts() {
       FILE *file = fopen("contact.txt", "r");
@@ -257,7 +292,7 @@ void loadContacts() {
        }
 
      while(fscanf(file, "%d,%49[^,],%49[^,],%14[^\n]\n", 
-     			&users[size].id, users[size].name, users[size].email, users[size].address, users[size].phone) != EOF) {
+     			&addre[size].id, addre[size].name, addre[size].email, addre[size].address, addre[size].phone) != EOF) {
      size++;
      }
      
